57ca974c8058526f75922db4dbc32d9a129a25bc
Tyler Yasaka <hey@tyleryasaka.me>
2018-03-20 15:18:26 -0700

    User cors package to allow all CORS requests

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var cors = require('cors')
 
 var twitter = require('./routes/twitter')
 
 module.exports = function() {
   var app = express()
 
+  app.use(cors())
   app.use(bodyParser.json())
   app.use('/api/twitter', twitter())
 
