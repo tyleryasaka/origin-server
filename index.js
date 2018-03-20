e955a32fd55519fe474d84a99899601aa700d3e0
Tyler Yasaka <hey@tyleryasaka.me>
2018-03-20 12:03:55 -0700

    Change default port to 4000
    This way it doesn't conflict with local development using demo-dapp on
    port 3000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var createApp = require('./app')
 var http = require('http')
 
 var app = createApp()
-var port = process.env.PORT || '3000'
+var port = process.env.PORT || '4000'
 var server = http.createServer(app)
 
 server.listen(port)
