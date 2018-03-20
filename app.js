86af4de99030dc27f25bf444b8739c2eabf0fc28
Tyler Yasaka <hey@tyleryasaka.me>
2018-03-20 12:02:29 -0700

    Use json body parser and add error handler

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 
 var twitter = require('./routes/twitter')
 
 module.exports = function() {
   var app = express()
 
+  app.use(bodyParser.json())
   app.use('/api/twitter', twitter())
+
+  app.use(function(err, req, res, next) {
+    console.error('error', err)
+    res.sendStatus(500)
+  })
 
   return app
 }
