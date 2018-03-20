72a05e268cbf1b51fff506fb08ed0fafbe1624d7
Tyler Yasaka <hey@tyleryasaka.me>
2018-03-20 12:03:03 -0700

    Add endpoint to post twitter status update

--- routes/twitter.js
+++ routes/twitter.js
@@ -1,9 +1,23 @@
 var express = require('express')
 var router = express.Router()
+var Twitter = require('twitter')
 
 module.exports = function() {
-  router.get('/foo', function(req, res) {
-    res.send('bar')
+  router.post('/statuses/update', function(req, res, next) {
+    let { accessTokenKey, accessTokenSecret, status } = req.body
+    let client = new Twitter({
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token_key: accessTokenKey,
+      access_token_secret: accessTokenSecret
+    })
+    client.post('statuses/update', { status })
+    .then(function (tweet) {
+      res.send(tweet.id_str)
+    })
+    .catch(function (error) {
+      next(error)
+    })
   })
   return router
 }
